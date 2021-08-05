a127de81baae848d5ac6608756bf63599b04f089
Andrew Behery <andrewbehery@gmail.com>
2021-08-04 20:21:09 -0500

    Set up Cypress E2E tests

---
+++ src/tests/index.test.tsx
@@ -0,0 +1,16 @@
+import {screen} from '@testing-library/react';
+import {getPage} from 'next-page-tester';
+import App from '../../pages/index';
+
+describe('App', () => {
+  it('renders without crashing', async () => {
+    const {render} = await getPage({
+      route: '/',
+    });
+
+    render();
+    expect(
+      screen.getByRole('heading', {name: 'Welcome to Next.js!'}),
+    ).toBeInTheDocument();
+  });
+});
