6fb924c2b3d61506ac4266f1c5ad9ae90aac008b
Andrew Behery <andrewbehery@gmail.com>
2021-08-04 23:20:42 -0500

    Delete sample content

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   reactStrictMode: true,
 };
+
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  /* config options here */
+};
+
+module.exports = nextConfig;
